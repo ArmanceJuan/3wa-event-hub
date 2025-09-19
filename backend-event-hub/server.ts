0ab318082b6c1c1736ab1b0df7926828d3f2f658
ArmanceJuan <armance.juan@gmail.com>
2025-09-19 11:36:35 +0200

    fix: b-correction for install backend correctly

--- backend-event-hub/server.ts
+++ backend-event-hub/server.ts
@@ -1,3 +1,24 @@
-const PORT = 3000;
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
 
-console.log(`Backend server started on port ${PORT}`);
+const app = express();
+const PORT = 3000;
+const MONGODB_URI =
+  "mongodb://esportroot:password1234@mongo:27017/esports?authSource=admin";
+
+app.use(cors());
+app.use(express.json());
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Could not connect to MongoDB", err));
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Home route of your backend!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
